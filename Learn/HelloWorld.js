2acc7373995b82b8951a3431446eca501d251e4f
Chaitanya Vadloori <chaitanya.vadloori@sentaca.com>
2021-10-26 13:22:37 -0400

    FirstPush

---
+++ Learn/HelloWorld.js
@@ -0,0 +1,10 @@
+
+
+function reSum()
+        {
+            const num1 = parseInt(document.getElementById("Num1").value);
+            const num2 = parseInt(document.getElementById("Num2").value);
+            document.getElementById("Sum").value = num1 + num2;
+
+        }
+
